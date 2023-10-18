ce99cdb5673772062aa404ee6240641eff9cd6b7
day0xy <day0xyz1@gmail.com>
2023-10-18 08:59:52 +0800

    测试flag

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"github.com/day0xy/Xscan/utils"
+	flag "github.com/spf13/pflag"
+)
+
+func main() {
+	flag.Parse()
+	params := utils.Params
+	if params.Help {
+		flag.PrintDefaults()
+	}
+
+}
