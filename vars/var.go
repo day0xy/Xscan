75ad8f185eb0db8e7280021a26a6bb7d07e014a6
day0xy <day0xyz1@gmail.com>
2023-10-18 08:45:04 +0800

    add var.go

---
+++ vars/var.go
@@ -0,0 +1,13 @@
+package vars
+
+type Params struct {
+	IpStr    string
+	PortStr  string
+	ScanType string
+	Help     bool
+	//思路： 如果指定了值，就把结构体里的这两个值，赋值到connect和syn的结构体里去
+	Thread  int
+	TimeOut int
+	//思路： bool变量来看是否启用ping
+	Pn bool
+}
