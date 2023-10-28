656e3b6fcda87e16d7e976ba2ec356d194cab398
day0xy <day0xyz1@gmail.com>
2023-10-28 10:46:00 +0800

    move common vars

--- scan/vars.go
+++ scan/vars.go
@@ -1,1 +1,19 @@
 package scan
+
+type PortJob struct {
+	ip   string
+	port int
+}
+
+type PortState uint8
+
+// 枚举类型
+const (
+	PortOpen PortState = iota
+	PortClosed
+)
+
+type Result struct {
+	Host  string
+	Ports map[int]PortState
+}
